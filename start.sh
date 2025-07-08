c1d440c5474bac8765fbd0e362b168b57c21550c
manojkumargogula <mr.gmanojkumar@gmail.com>
2025-07-08 09:53:41 +0000

    refactor: rearrange startup sequence in start.sh to include NLTK setup

--- start.sh
+++ start.sh
@@ -7,19 +7,18 @@ echo 'killing existing docker containers...'
 docker-compose down
 
 echo 'starting kafka...'
-
 docker-compose up -d
 
 echo 'waiting for kafka to start...'
-
 sleep 10
 
-echo 'starting producer...'
+echo 'setup nlkt...'
+python3 setup_nltk.py
 
+echo 'starting producer...'
 tmux new-session -d -s producer 'python3 producer/producer.py'
 
 echo 'starting consumer...'
-
 tmux new-session -d -s consumer 'python3 consumer/consumer.py'
 
 tmux attach-session -d -s consumer
